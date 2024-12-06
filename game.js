1e811dc28cd29f2c3c549e57e6c5b0bac191c2bb
Samuel Rodriguez <sam.rod.719@gmail.com>
2024-12-06 14:29:08 -0800

    add next sequence function and button handler

--- game.js
+++ game.js
@@ -1,3 +1,18 @@
+const buttonColors = ["yellow", "green", "red", "blue"];
+const gamePattern = [];
+const userPattern = [];
+
 const nextSequence = () => {
   const randomNumber = Math.floor(Math.random() * 4);
+  const randomColor = buttonColors[randomNumber];
+  gamePattern.push(randomColor);
+  const audio = new Audio(`./sounds/${randomColor}.mp3`);
+  audio.play();
+  $(`.${randomColor}`).fadeOut(200).fadeIn(200);
 };
+
+$(".button").on("click", (event) => {
+  const userChosenColor = event.target.id;
+  userPattern.push(userChosenColor);
+  console.log(userPattern);
+});
